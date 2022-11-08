2c7ec2ad9e7c386d15bf3ecbf8f430e68b96c657
Erik-D-Nelson <erik.nelson116@gmail.com>
2022-11-07 23:40:37 -0500

    heatmaps for fig4

---
+++ code/in_progress_unsorted/fig4_heatmaps.R
@@ -0,0 +1,61 @@
+library(pheatmap)
+
+load('de_dge.rda')
+
+##pull out the FDRs for heatmaps
+heat_all<-data.frame(
+  'GC'<-de.dge[[11]]$FDR,
+  'CA3'<-de.dge[[3]]$FDR,
+  'PS'<-de.dge[[15]]$FDR,
+  'CA1'<-de.dge[[1]]$FDR,
+  row.names=rownames(de.dge[[1]])
+)
+colnames(heat_all)<-c('GC','CA3','PS','CA1')
+
+##Set anything NA=1
+heat_all[is.na(heat_all)]<-1
+
+## -log10 transform
+heat_all=-log10(heat_all)
+
+##gene lists for heatmaps
+synapse<-c('Grin2a','Sorcs3','Dlg4','Ntng2','Baiap2',
+           'Grasp','Phactr1','Svop','Sv2b','Slc8a1')
+
+
+signal<-c('Ntrk2','Bdnf','Brinp1','Ptgs2','Il16',
+          'Tgfb2','Sh3gl2','Slit2','Gpr68','Pde4b')
+
+
+transfer<-c('Plk2','Akap13','Pim1','Cop1','Nrp1',
+            'Rps6ka4','Limk1','Nedd4l','Itpk1','Mast3')
+
+##make heatmaps
+heat_syn<-heat_all[rownames(heat_all) %in% synapse,]
+heat_syn<-heat_syn[match(synapse,rownames(heat_syn)),]
+
+heat_sig<-heat_all[rownames(heat_all) %in% signal,]
+heat_sig<-heat_sig[match(signal,rownames(heat_sig)),]
+
+heat_trans<-heat_all[rownames(heat_all) %in% transfer,]
+heat_trans<-heat_trans[match(transfer,rownames(heat_trans)),]
+
+
+pdf('synapse.pdf',h=3,w=3)
+pheatmap(heat_syn,cluster_cols=F,cluster_rows=F,
+         scale='row',angle_col='45',fontsize=11)
+dev.off()
+
+pdf('signal.pdf',h=3,w=3)
+pheatmap(heat_sig,cluster_cols=F,cluster_rows=F,
+         scale='row',angle_col='45',fontsize=11)
+dev.off()
+
+pdf('transfer.pdf',h=3,w=3)
+pheatmap(heat_transfer,cluster_cols=F,cluster_rows=F,
+         scale='row',angle_col='45',fontsize=11)
+dev.off()
+
+
+
+
