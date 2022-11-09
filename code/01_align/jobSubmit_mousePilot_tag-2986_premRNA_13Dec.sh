6c7faf898bda36d3cdc5d07efa4d9452e7589299
Erik-D-Nelson <erik.nelson116@gmail.com>
2022-11-09 17:04:33 -0500

    adding bash scripts for cellranger from MNT

---
+++ code/01_align/jobSubmit_mousePilot_tag-2986_premRNA_13Dec.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -N jobSubmit_tag-2986_hpc_premRNA_MNT
+#$ -V
+#$ -pe local 8
+#$ -wd /dcl01/ajaffe/data/lab/singleCell/mouse_10x/premRNA/
+#$ -l mem_free=18G,h_vmem=20G,h_fsize=200G
+#$ -o ./logs/jobSubmit_tag-2986_hpc_premRNA_MNT_log.txt
+#$ -e ./logs/jobSubmit_tag-2986_hpc_premRNA_MNT_log.txt
+
+/dcl01/ajaffe/data/lab/singleCell/cellranger-3.0.2/cellranger count --id=tag2986_hpc_premRNA \
+		 --transcriptome=/dcl01/ajaffe/data/lab/singleCell/mouse_10x/premRNA/mm10-3.0.0_premrna \
+		 --fastqs=/dcl01/ajaffe/data/lab/singleCell/mouse_10x/FASTQ/2986/ \
+		 --sample=2986 \
+		 --jobmode=local \
+		 --localcores=8 \
+		 --localmem=144
