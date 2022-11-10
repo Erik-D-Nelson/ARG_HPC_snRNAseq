0ca71019edde3dc1bee9f07712f8eceb0844a44b
Erik-D-Nelson <erik.nelson116@gmail.com>
2022-11-10 18:56:22 -0500

    gsea plot edits

--- code/06_gsea/gsea_eda_and_plots.R
+++ code/06_gsea/gsea_eda_and_plots.R
@@ -233,8 +233,10 @@ for(i in 1:length(bp)){
 bp_fdr_df<-do.call(cbind.data.frame, fdr_bp)
 bp_fdr_df[is.na(bp_fdr_df)]<-1
 bp_fdr_df= -log10(bp_fdr_df)
+colnames(bp_fdr_df)<-names(de.dge)
 bp_fdr_df$mean<-rowMeans(bp_fdr_df)
 rownames(bp_fdr_df)<-bp_total
+
 
 bp_parent_df<-list()
 bp_means<-list()
@@ -248,8 +250,27 @@ for(i in 1:length(group_bp)){
 bp_median[[i]]<-median(bp_parent_df[[i]]$mean)
 }
 
+count_bp<-list()
+for(i in 1:length(bp)){
+  count_bp[[i]]<-bp[[i]]$Count
+}
+count_bp[[16]]<-rep(NA,814)
+
+bp_count_df<-do.call(cbind.data.frame, count_bp)
+bp_count_df[is.na(bp_count_df)]<-0
+colnames(bp_count_df)<-names(de.dge)
+rownames(bp_count_df)<-bp_total
+
+
+bp_parent_count<-list()
+for(i in 1:length(group_bp)){
+  bp_parent_count[[i]]<-bp_count_df[rownames(bp_count_df) %in% group_bp[[i]],]
+}
+
+
 
 names(bp_parent_df)<-names(group_bp)
+names(bp_parent_count)<-names(group_bp)
 names(bp_means)<-names(group_bp)
 names(bp_median)<-names(group_bp)
 
@@ -266,8 +287,10 @@ for(i in 1:length(cc)){
 cc_fdr_df<-do.call(cbind.data.frame, fdr_cc)
 cc_fdr_df[is.na(cc_fdr_df)]<-1
 cc_fdr_df= -log10(cc_fdr_df)
+colnames(cc_fdr_df)<-names(de.dge)
 cc_fdr_df$mean<-rowMeans(cc_fdr_df)
 rownames(cc_fdr_df)<-cc_total
+
 
 cc_parent_df<-list()
 cc_means<-list()
@@ -281,7 +304,24 @@ for(i in 1:length(group_cc)){
   cc_median[[i]]<-median(cc_parent_df[[i]]$mean)
 }
 
+count_cc<-list()
+for(i in 1:length(cc)){
+  count_cc[[i]]<-cc[[i]]$Count
+}
+
+cc_count_df<-do.call(cbind.data.frame, count_cc)
+cc_count_df[is.na(cc_count_df)]<-0
+colnames(cc_count_df)<-names(de.dge)
+rownames(cc_count_df)<-cc_total
+
+
+cc_parent_count<-list()
+for(i in 1:length(group_cc)){
+  cc_parent_count[[i]]<-cc_count_df[rownames(cc_count_df) %in% group_cc[[i]],]
+}
+
 names(cc_parent_df)<-names(group_cc)
+names(cc_parent_count)<-names(group_cc)
 names(cc_means)<-names(group_cc)
 names(cc_median)<-names(group_cc)
 
@@ -298,8 +338,10 @@ for(i in 1:length(mf)){
 mf_fdr_df<-do.call(cbind.data.frame, fdr_mf)
 mf_fdr_df[is.na(mf_fdr_df)]<-1
 mf_fdr_df= -log10(mf_fdr_df)
+colnames(mf_fdr_df)<-names(de.dge)
 mf_fdr_df$mean<-rowMeans(mf_fdr_df)
 rownames(mf_fdr_df)<-mf_total
+
 
 mf_parent_df<-list()
 mf_means<-list()
@@ -313,13 +355,123 @@ for(i in 1:length(group_mf)){
   mf_median[[i]]<-median(mf_parent_df[[i]]$mean)
 }
 
+count_mf<-list()
+for(i in 1:length(mf)){
+  count_mf[[i]]<-mf[[i]]$Count
+}
+count_mf[[2]]<-rep(NA,97)
+count_mf[[4]]<-rep(NA,97)
+count_mf[[10]]<-rep(NA,97)
+count_mf[[11]]<-rep(NA,97)
+count_mf[[15]]<-rep(NA,97)
+
+mf_count_df<-do.call(cbind.data.frame, count_mf)
+mf_count_df[is.na(mf_count_df)]<-0
+colnames(mf_count_df)<-names(de.dge)
+rownames(mf_count_df)<-mf_total
+
+
+mf_parent_count<-list()
+for(i in 1:length(group_mf)){
+  mf_parent_count[[i]]<-mf_count_df[rownames(mf_count_df) %in% group_mf[[i]],]
+}
+
+
+
 names(mf_parent_df)<-names(group_mf)
+names(mf_parent_count)<-names(group_mf)
 names(mf_means)<-names(group_mf)
 names(mf_median)<-names(group_mf)
 
 
+##heatmaps of all clusters
+
+bp_heats<-list()
+for(i in 1:length(bp_parent_df)){
+  bp_heats[[i]]<-pheatmap(bp_parent_df[[i]],cluster_rows = F,cluster_cols = F,
+                          scale='row')
+}
+
+cc_heats<-list()
+for(i in 1:length(cc_parent_df)){
+  cc_heats[[i]]<-pheatmap(cc_parent_df[[i]],cluster_rows = F,cluster_cols = F,
+                          scale='row')
+}
 
 
+mf_heats<-list()
+for(i in 1:length(mf_parent_df)){
+  mf_heats[[i]]<-pheatmap(mf_parent_df[[i]],cluster_rows = F,cluster_cols = F,
+                          scale='row')
+}
+
+
+###OK now lets make some plotskis
+##CC we'll plot synapse and secretory vesicle
+synapse<-cc_parent_df[[1]]
+synapse<-synapse[order(synapse$mean,decreasing=T),]
+rownames(synapse)<-go2term(rownames(synapse))$Term
+synapse<-synapse[,1:17]
+colnames(synapse)<-c("CA1", "CA2", "CA3.1", "CA3.2", "CA4",
+  "DG", "GABA.1", "GABA.2", "GABA.3", "GABA.4",
+  "GABA.5", "PS.1", "PS.2", "L2/3", "L5/Po",
+  "L6/6b", "Sub")
+synapse<-synapse[,c(6,3,12,1,9,13,11,4,17,7,8,10,5,14,16,2,15)]
+
+synapse_count<-cc_parent_count[[1]]
+rownames(synapse_count)<-go2term(rownames(synapse_count))$Term
+synapse_count<-synapse_count[match(rownames(synapse),rownames(synapse_count)),]
+synapse_count<-synapse_count[,1:17]
+colnames(synapse_count)<-c("CA1", "CA2", "CA3.1", "CA3.2", "CA4",
+                           "DG", "GABA.1", "GABA.2", "GABA.3", "GABA.4",
+                           "GABA.5", "PS.1", "PS.2", "L2/3", "L5/Po",
+                           "L6/6b", "Sub")
+synapse_count<-synapse_count[,c(6,3,12,1,9,13,11,4,17,7,8,10,5,14,16,2,15)]
+
+
+synapse<-synapse[c(1:5),]
+synapse_count<-synapse_count[c(1:5),]
+synapse$ID<-factor(rownames(synapse))
+synapse_melt<-melt(synapse)
+
+
+ggplot(synapse_melt, aes(x =variable, y = ID)) + 
+  geom_point(aes(colour=value,size=unlist(synapse_count))) +
+  theme(panel.background=element_blank(),
+        panel.border = element_rect(colour = "black", 
+                                    fill=NA), 
+        axis.text.x = element_text(angle = 45,
+                                   vjust=.5)) + 
+  scale_colour_gradient(low='white',
+                        high='black') +
+  scale_size_area(breaks=c(0,50,100,150,200))+
+  labs(size='Count',
+       color='-log10 adjusted P value',
+       x='Annotation',
+       y='GO Term')
+dev.off()
+
+
+ggplot(ion_melt, aes(x =variable, y = ID)) + 
+  geom_point(aes(colour=value,size=unlist(key_ion))) +
+  theme(panel.background=element_blank(),
+        panel.border = element_rect(colour = "black", 
+                                    fill=NA), 
+        axis.text.x = element_text(angle = 45,
+                                   vjust=.5),
+        text = element_text(size = 12)) + 
+  scale_colour_gradient(low='white',
+                        high='black') +
+  scale_size_area(breaks=c(0,50,100,150,200))+
+  labs(size='Count',
+       color='-log10 adjusted P value',
+       x='Annotation',
+       y='GO Term') + 
+  ggtitle('Transcription factor binding')
+
+
+
+  reducedTerms_CC$term[reducedTerms_cc$parentTerm=='glutamatergic synapse']
 
 
 
@@ -343,29 +495,26 @@ tgfb<-c('cellular response to chemical stress',
         'cellular response to oxidative stress',
         'response to cytokine',
         'transforming growth factor beta receptor signaling pathway')
-bp_plot<-list()
-for(i in 1:length(bp)){
-  ifelse(nrow(bp[[i]] > 0),
-  bp_plot[[i]]<-bp[[i]][,c(2,7,12)],
-  bp_plot[[i]]<-bp[[i]][,c(2,7)])
+cc_plot<-list()
+for(i in 1:length(cc)){
+
   
-  bp_plot[[i]]<-bp_plot[[i]][bp_plot[[i]]$Description %in% c(signal),]
+  cc_plot[[i]]<-cc[[i]][cc[[i]]$Description %in% c(rownames(synapse)),]
 }
 
 
 
-bp_plot_final<-bp_plot
-for(i in 1:length(bp_plot)){
-  bp_plot_final[[i]]<-bp_plot_final[[i]][match(signal,
-                                               bp_plot_final[[i]]$Description),]
-  bp_plot_final[[i]]$Description<-factor(bp_plot_final[[i]]$Description)
+cc_plot_final<-cc_plot
+for(i in 1:length(cc_plot_final)){
+  cc_plot_final[[i]]<-cc_plot_final[[i]][match(synapse,
+                                               cc_plot_final[[i]]$Description),]
+  cc_plot_final[[i]]$Description<-factor(cc_plot_final[[i]]$Description)
 }
 
-bp_plot_final[[16]]$Count<-rep(NA,9)
 
-key<-lapply(bp_plot_final,pull,Count)
-keydf<-data.frame('ID'=signal)
-keydf[,2:18]<-key
+key<-lapply(cc_plot_final,pull,Count)
+keydf<-data.frame('ID'=synapse)
+keydf<-key
 keydf[is.na(keydf)]<-0
 keydf$ID<-NULL
 key_signal<-keydf
@@ -398,7 +547,7 @@ bp_signal<-recode(bp_signal,variable=Annotation)
 
 pdf('signaling.pdf',h=6,w=8)
 ggplot(signal_melt, aes(x =variable, y = ID)) + 
-  geom_point(aes(colour=value,size=unlist(key_signal))) +
+  geom_point(aes(colour=value,size=unlist(value))) +
   theme(panel.background=element_blank(),
         panel.border = element_rect(colour = "black", 
                                     fill=NA), 
