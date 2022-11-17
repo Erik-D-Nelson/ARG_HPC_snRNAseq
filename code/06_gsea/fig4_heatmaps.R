120902cde5ccd1b968c4ca85a4e7725320bba014
Erik-D-Nelson <erik.nelson116@gmail.com>
2022-11-17 16:42:55 -0500

    Editing heatmaps for fig4

--- code/06_gsea/fig4_heatmaps.R
+++ code/06_gsea/fig4_heatmaps.R
@@ -1,4 +1,5 @@
 library(pheatmap)
+library(org.Mm.eg.db)
 
 load('de_dge.rda')
 
@@ -11,23 +12,45 @@ heat_all<-data.frame(
   row.names=rownames(de.dge[[1]])
 )
 colnames(heat_all)<-c('GC','CA3','PS','CA1')
+#heat_all[is.na(heat_all)]<-0
+
+syn<-rownames(cc_parent_df[[1]])
+growth<-rownames(bp_parent_df[[11]])
+ubiq<-rownames(mf_parent_df[[8]])
+kinases<-rownames(mf_parent_df[[2]])
+
+GC<-de.dge[[6]][rownames(de.dge[[6]]) %in% DEG.GC,]
+CA1<-de.dge[[1]][rownames(de.dge[[1]]) %in% DEG.CA1,]
+CA3<-de.dge[[3]][rownames(de.dge[[3]]) %in% DEG.CA3,]
+PS<-de.dge[[12]][rownames(de.dge[[12]]) %in% DEG.PS,]
+
+##function to find genes with heterogeneous DE patterns across cell types
+find_genes<-function(x,y,z){
+  table<-relocate(heat_all,x)
+  table<-table[rownames(table) %in% y,]
+  table$meanRatio<-table[,1]-pmax(table[,2],table[,3],table[,4])
+  table<-table[order(table$meanRatio,decreasing=T),]
+  retrieved <- AnnotationDbi::select(org.Mm.eg.db, keytype="GO", 
+                                     keys=z, columns="SYMBOL")$SYMBOL
+  table<-table[rownames(table) %in% retrieved,]
+  return(table)
+}
 
 
 ##gene lists for heatmaps
-synapse<-c('Grin2a','Sorcs3','Dlg4','Ntng2','Baiap2',
-           'Grasp','Ppm1h','Phactr1','Svop','Sv2b','Slc8a1')
+synapse<-c('Arc','Nptx2','Grin2a','Sorcs3','Dlg4','Baiap2','Iqsec3',
+           'Grasp','Ppm1h','Phactr1','Svop','Sv2b','Sh3gl2','Slc8a1')
 
 
-signal<-c('Ntrk2','Bdnf','Brinp1','Ptgs2','Tgfb2','Ednrb','Gpr176',
-          'Sh3gl2','Gpr68','Pde4b')
+signal<-c('Inhba','Ptgs2','Bdnf','Ntrk2','Tgfb2','Rhoq',
+          'Smad3','Gclc','Gcnt2','Twf2','Pde4b','Nos1')
 
 
-transfer<-c('Cop1','Ube2g1','Cul3','Ube2q2','Ubr5','Mib1',
-            'Bcor','Ube2g2','Nedd4l','Trim71')
+transfer<-c('Cop1','Fbxw7','Ube2g1','Cul3','Ube2q2','Mib1',
+            'Znrf3','Rnf38','Bcor','Rmnd5b','Rnf150','Nedd4l')
 
-kinase<-c('Pim1','Nrp1','Plk2','Akap13','Mapk4','Hunk',
-          'Cdkn1a','Itpk1','Mapk11','Csnk2b')
-
+kinase<-c('Pim1','Plk2','Akap13','Mapk4','Stk40','Rps6ka3',
+          'Pi4ka','Tlk1','Itpka','Igf1r','Mapk11','Itpk1')
 ##make heatmaps
 heat_syn<-heat_all[rownames(heat_all) %in% synapse,]
 heat_syn<-heat_syn[match(synapse,rownames(heat_syn)),]
@@ -41,27 +64,9 @@ heat_trans<-heat_trans[match(transfer,rownames(heat_trans)),]
 heat_kinase<-heat_all[rownames(heat_all) %in% kinase,]
 heat_kinase<-heat_kinase[match(kinase,rownames(heat_kinase)),]
 
+heat<-rbind(heat_syn,heat_sig,heat_trans,heat_kinase)
 
-pdf('synapse.pdf',h=2.5,w=2.3)
-pheatmap(heat_syn,cluster_cols=F,cluster_rows=F,
+pdf('heat_all.pdf',h=8.5,w=3)
+pheatmap(heat,cluster_cols=F,cluster_rows=F,
          scale='row',angle_col='45',fontsize=11,na_col='darkblue')
 dev.off()
-
-pdf('signal.pdf',h=2.2,w=2.2)
-pheatmap(heat_sig,cluster_cols=F,cluster_rows=F,
-         scale='row',angle_col='45',fontsize=11,na_col='darkblue')
-dev.off()
-
-pdf('transfer.pdf',h=2.2,w=2.2)
-pheatmap(heat_trans,cluster_cols=F,cluster_rows=F,
-         scale='row',angle_col='45',fontsize=11,na_col='darkblue')
-dev.off()
-
-pdf('kinase.pdf',h=2.2,w=2.2)
-pheatmap(heat_kinase,cluster_cols=F,cluster_rows=F,
-         scale='row',angle_col='45',fontsize=11,na_col='darkblue')
-dev.off()
-
-
-
-
